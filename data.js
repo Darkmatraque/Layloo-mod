85fcc811890659c75e473d17f00c50e10c663ac2
Darkmatraque <kevin2007baumann@gmail.com>
2026-04-13 02:47:00 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: 1,
+    pseudo: "Viewer1",
+    msgDeleted: 12,
+    timeouts: 3,
+    banDef: false
+  },
+  {
+    id: 2,
+    pseudo: "Viewer2",
+    msgDeleted: 0,
+    timeouts: 0,
+    banDef: true
+  }
+];
